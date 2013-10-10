24bd99bfbabd7da5ac626b13ad71b2d466180223
Dino Wernli <dinowernli@google.com>
2013-10-10 13:56:39 +0200

    Revert to old fff bot

--- src/main/FetchAndFireBot.java
+++ src/main/FetchAndFireBot.java
@@ -1,41 +1,43 @@
 package main;
 
-import java.util.Collections;
-import java.util.List;
-
 import se.openmind.kart.GameState;
 import se.openmind.kart.GameState.ItemBox;
 import se.openmind.kart.GameState.Kart;
 import se.openmind.kart.OrderUpdate.Order;
-
-import com.google.common.base.Optional;
 
 public class FetchAndFireBot extends MooBot {
   @Override
   public Order playGame(GameState state) {
     Kart me = state.getYourKart();
 
-    List<Kart> sortedKarts = state.getEnemyKarts();
-    Collections.sort(sortedKarts, new DistanceToEntityComparator(state.getYourKart()));
-    List<ItemBox> sortedBoxes = state.getItemBoxes();
-    Collections.sort(sortedBoxes, new DistanceToEntityComparator(state.getYourKart()));
-
-    if (me.getShells() == 0) {
-      return moveTo(sortedBoxes.get(0));
-    }
-
-    for (Kart kart : sortedKarts) {
-      if (isGuaranteedHit(me, kart)) {
-        return Order.FireOrder(kart.getId());
+    if (me.getShells() < 2) {
+      // This default implementation will move towards the closest item
+      // box
+      ItemBox closestItemBox = null;
+      for (ItemBox i : state.getItemBoxes()) {
+        if (closestItemBox == null || distance(me, i) < distance(me, closestItemBox)) {
+          closestItemBox = i;
+        }
+      }
+      if (closestItemBox != null) {
+        return Order.MoveOrder(closestItemBox.getXPos(), closestItemBox.getYPos());
+      }
+    } else {
+      Kart closestKart = null;
+      for (Kart k : state.getEnemyKarts()) {
+        if (closestKart == null || distance(me, k) < distance(me, closestKart)) {
+          closestKart = k;
+        }
+      }
+      if (closestKart != null) {
+        if (isGuaranteedHit(me, closestKart)) {
+          return Order.FireOrder(closestKart.getId());
+        } else {
+          return Order.MoveOrder(closestKart.getXPos(), closestKart.getYPos());
+        }
       }
     }
 
-    Optional<ItemBox> itemBox = selectItemBox(state);
-    if (itemBox.isPresent()) {
-      return Order.MoveOrder(itemBox.get().getXPos(), itemBox.get().getYPos());
-    }
-
-    Kart targetKart = sortedKarts.get(0);
-    return Order.MoveOrder(targetKart.getXPos(), targetKart.getYPos());
+    return null;
   }
 }
