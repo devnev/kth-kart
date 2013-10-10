1fb0f27ac6831f0ebb5fa9feaab5f2db703d522d
Mark Nevill <nev@google.com>
2013-10-10 11:58:42 +0200

    Formatting only.

--- src/main/FetchAndFireBot.java
+++ src/main/FetchAndFireBot.java
@@ -9,48 +9,44 @@ import se.openmind.kart.OrderUpdate.Order;
 
 public class FetchAndFireBot implements Bot {
 
-	@Override
-	public Order playGame(GameState state) {
-		Kart me = state.getYourKart();
+  @Override
+  public Order playGame(GameState state) {
+    Kart me = state.getYourKart();
 
-		if (me.getShells() == 0) {
-			// This default implementation will move towards the closest item
-			// box
-			ItemBox closestItemBox = null;
-			for (ItemBox i : state.getItemBoxes()) {
-				if (closestItemBox == null
-						|| distance(me, i) < distance(me, closestItemBox)) {
-					closestItemBox = i;
-				}
-			}
-			if (closestItemBox != null) {
-				return Order.MoveOrder(closestItemBox.getXPos(),
-						closestItemBox.getYPos());
-			}
-		} else {
-			Kart closestKart = null;
-			for (Kart k : state.getEnemyKarts()) {
-				if (closestKart == null
-						|| distance(me, k) < distance(me, closestKart)) {
-					closestKart = k;
-				}
-			}
-			if (closestKart != null) {
-				if (distance(me, closestKart) < 30) {
-					return Order.FireOrder(closestKart.getId());
-				} else {
-					return Order.MoveOrder(closestKart.getXPos(),
-							closestKart.getYPos());
-				}
-			}
-		}
+    if (me.getShells() == 0) {
+      // This default implementation will move towards the closest item
+      // box
+      ItemBox closestItemBox = null;
+      for (ItemBox i : state.getItemBoxes()) {
+        if (closestItemBox == null || distance(me, i) < distance(me, closestItemBox)) {
+          closestItemBox = i;
+        }
+      }
+      if (closestItemBox != null) {
+        return Order.MoveOrder(closestItemBox.getXPos(), closestItemBox.getYPos());
+      }
+    } else {
+      Kart closestKart = null;
+      for (Kart k : state.getEnemyKarts()) {
+        if (closestKart == null || distance(me, k) < distance(me, closestKart)) {
+          closestKart = k;
+        }
+      }
+      if (closestKart != null) {
+        if (distance(me, closestKart) < 30) {
+          return Order.FireOrder(closestKart.getId());
+        } else {
+          return Order.MoveOrder(closestKart.getXPos(), closestKart.getYPos());
+        }
+      }
+    }
 
-		return null;
-	}
+    return null;
+  }
 
-	private double distance(Entity a, Entity b) {
-		double xDist = a.getXPos() - b.getXPos();
-		double yDist = a.getYPos() - b.getYPos();
-		return Math.sqrt(xDist * xDist + yDist * yDist);
-	}
+  private double distance(Entity a, Entity b) {
+    double xDist = a.getXPos() - b.getXPos();
+    double yDist = a.getYPos() - b.getYPos();
+    return Math.sqrt(xDist * xDist + yDist * yDist);
+  }
 }
